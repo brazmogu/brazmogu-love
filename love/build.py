0bc744c81b7432c902e9a84194935a8748da3bde
Bruno Guedes <guedesav@gmail.com>
2019-08-09 09:46:28 -0300

    [DRAFT] Build script drafted

---
+++ love/build.py
@@ -0,0 +1,32 @@
+# Build generator for the current iteration of the BrazMogu LÖVE2D sub-engine
+
+def love2d_linux():
+    # Extract Linux LÖVE binary
+    # Build the new LÖVE binary
+    # Re-pack
+    pass
+
+def love2d_win64():
+    # Extract Win64 LÖVE EXE
+    # Build the new LÖVE EXE
+    # Re-pack
+    pass
+
+def love2d_win32():
+    # Extract Win32 LÖVE EXE
+    # Build the new LÖVE EXE
+    # Re-pack
+    pass
+
+def love2d_macos():
+    # Extract Mac OSX LÖVE package
+    # Build the new LÖVE package
+    # Re-pack
+    pass
+
+# Default output location (relative to project root)
+OUTPUT="output"
+
+DEBUG=True
+
+# TODO: implement main body and generate output
