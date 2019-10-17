da7832007467178df0e275940017dada37701bb9
Bruno Guedes <guedesav@gmail.com>
2019-10-16 21:31:28 -0300

    ScreenStack implementation

--- love/src/brazmogu/screen.lua
+++ love/src/brazmogu/screen.lua
@@ -2,3 +2,77 @@
 -- A screen is a generic abstraction for a game's environment. It may be a scene, a level, a menu...
 -- anything that contains logic and actors that the game is running.
 -- Scenes are instanced and handled in the Screen Stack, and may contain actors and components.
+
+local Screen = {}
+
+Screen._meta = {
+    __index = Screen
+}
+
+--- Creates a new screen
+-- @tparam table args Argument table
+-- @tparam boolean args.transparent Transparency flag (default: false)
+-- @return A new Screen instance
+function Screen.new(args)
+    -- Initialize instance data
+    local screen = {
+        actors = {}, -- Actor list
+        components = {} -- Component list,
+        transparent = args.transparent or false -- Transparency flag
+    }
+
+    setmetatable(screen, Screen._meta)
+
+    return screen
+end
+
+--- Add an actor of given type to the Screen
+-- @param actorType Table describing the Actor type (see Actor)
+-- @param data Instance data
+function Screen:addActor(actorType, data)
+    local newActor = Actor.new(actorType, data)
+
+    table.insert(self.actors, newActor)
+end
+
+--- Add a component of given type to the Screen
+-- @param componentType Table describing the Component type (see Component)
+-- @param data Instance data
+function Screen:addComponent(componentType, data)
+    local newComponent = Component.new(componentType, data)
+
+    table.insert(self.components, newComponent)
+end
+
+--- Invoke a callback on this screen and every actor and component contained in the screen
+-- @param callback Callback name
+function Screen:invoke(callback, ...)
+    if self[callback] then
+        self:callback(...)
+    end
+
+    for _, actor in ipairs(actors) do
+        if actor[callback] then
+            actor:callback(...)
+        end
+    end
+
+    for _, component in iapirs(components) do
+        if component[callback] then
+            component:callback(...)
+        end
+    ends
+end
+
+--- @section Callbacks
+
+--- Update
+-- Runs a single tick
+-- @function update
+-- @tparam float dt Time elapsed in this tick
+
+--- Draw
+-- Draws on the screen
+-- @function draw
+
+return Screen
--- love/src/brazmogu/screenStack.lua
+++ love/src/brazmogu/screenStack.lua
@@ -3,3 +3,49 @@
 -- instanced on the top of the stack.
 -- By manipulating properties of the screen, the game may know how to handle updating and rendering the
 -- elements contained in said screens.
+
+local ScreenStack = {
+    screens = {},
+    top = nil
+}
+
+--- Adds a scren to the top of the stack.
+-- If the screen is already in the stack, nothing is done.
+-- @return The screen at the top of the stack.
+function ScreenStack:push(screen)
+    -- TODO: check the screen is not added yet
+    table.insert(self.screens, screen)
+    self.top = self.screens[#self.screens]
+    
+    return self.top
+end
+
+--- Remove the screen from the top of the stack.
+-- @return The screen that was removed from the stack.
+function ScreenStack:pop()
+    if #self.screens == 0 then
+        WARNING("Screen Stack is empty and cannot be popped.")
+        return nil
+    end
+
+    local popped = table.remove(self.screens)
+    self.top = self.screens[#self.screens]
+
+    return popped
+end
+
+--- Invoke callbacks for the screens at the top of the stack.
+-- The invoke is stopped at the first non-transparent screen.
+-- @param callback Callback function name
+function ScreenStack:invoke(callback, args)
+    for i = #self.screens,1,-1 do
+        local screen = self.screens[i]
+        screen:invoke(callback, args)
+
+        if not screen.transparent then
+            break
+        end
+    end
+end
+
+return ScreenStack
