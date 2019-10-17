7bb05c00fe8b433fb5a8a2e538355294042746d7
Bruno Guedes <guedesav@gmail.com>
2019-10-16 21:34:24 -0300

    Add more logic to main module
    - Add defaul game config
    - Add check on load
    - Add invoke for game loop functions

--- love/src/main.lua
+++ love/src/main.lua
@@ -3,27 +3,44 @@
 
 -- Global Requires and Modules
 require("brazmogu/core/debug")
+require("brazmogu/core/globals")
 
 -- Game Config File
 local gameConfig = require("game_config")
 if gameConfig then
 else
     DEBUG("Game Configuration should be defined in 'game_config.lua' in the project source root. Game will proceed with engine default values.")
+    gameConfig = {
+        main = "game"
+    }
 end
 
 -- LÖVE2D Callback definitions
 
 --- Load: pre-load stuff and instantiate the starting scene
-function love.load()
+function love.load(args)
+    INFO(package.path)
+    print(TABLE(args))
+
+    local Game = require(gameConfig.main)
+    if not Game then
+        ERROR("No Game module was found in the game package. Nothing to do, then!")
+        return
+    end
+
+    -- Load the game
+    Game.load()
 end
 
 --- Update: update everything in the game
 -- BrazMogu LÖVE's update cycle also handles input callbacks an other events in the game components
 function love.update(dt)
+    Global.screenStack:invoke("update", dt)
 end
 
 --- Draw: render stuff on the screen
 function love.draw()
+    Global.screenStack:draw("draw")
 end
 
 --- Run: game loop
