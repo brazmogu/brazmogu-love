56847e70e77d59d4f953c2c3e477ff2364f6cb6a
Bruno Guedes <guedesav@gmail.com>
2019-10-16 21:33:02 -0300

    Table print function implemented

--- love/src/brazmogu/core/debug.lua
+++ love/src/brazmogu/core/debug.lua
@@ -19,3 +19,28 @@ end
 function ERROR(...)
     print("[ERROR]", ...)
 end
+
+--- Formats table data as a string
+function TABLE(T, indent, visited)
+    indent = indent or 0
+    visited = visited or {}
+
+    if visited[T] then
+        return string.format("%s [...]", T)
+    end
+
+    visited[T] = true
+    local header = string.format("%s {", T)
+
+    local innerData = {}
+    for k,v in pairs(T) do
+        local index = string.format("[%s]", tostring(k))
+        if type(v) == "table" then
+            table.insert(innerData, string.format("%s: %s", index, TABLE(v, indent+1, visited)))
+        else
+            table.insert(innerData, string.format("%s: %s", index, tostring(v)))
+        end
+    end
+
+    return table.concat({header, "   " .. table.concat(innerData, "\n   "), "}"}, "\n")
+end
